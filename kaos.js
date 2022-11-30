3e6c127b5c41ed59ed34baaf6bc989b446fc36a8
Manuel Steinberg <github@manuel-steinberg.de>
2022-11-30 10:21:30 +0100

    Update kaos.js

--- kaos.js
+++ kaos.js
@@ -1,3 +1,11 @@
 function _id (id) { 
   return document.getElementById(id); 
 };
+
+function _tags (tag) {
+  return document.getElementsByTagName(tag);
+};
+
+function _classes (name) {
+  return document.getElementsByClassName(name)
+};
